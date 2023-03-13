800180245c7e08110a1507f6b7a57c79046bf04e
Vinícius Yoda <vyoda4604@gmail.com>
2023-03-13 17:10:14 -0300

    navigation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import useProdutores from './src/hooks/useProdutores';
 
 import Home from './src/telas/Home';
@@ -10,13 +11,15 @@ export default function App() {
 
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
-    <Home melhoresProdutores={false} />
-    {/* {produtores.length > 0 && 
-      <Cesta produtor={{
-        nome: produtores[0].nome, 
-        imagem: produtores[0].imagem
-      }}
-      {...produtores[0].cestas[0]} />
-    } */}
+    <NavigationContainer>
+      <Home melhoresProdutores={false} />
+      {/* {produtores.length > 0 && 
+        <Cesta produtor={{
+          nome: produtores[0].nome, 
+          imagem: produtores[0].imagem
+        }}
+        {...produtores[0].cestas[0]} />
+      } */}
+    </NavigationContainer>
   </SafeAreaView>
 }
